4119225f96a253ee4ac3b0e947e3112d2f6ad092
Luis Caraballo <96073778+Lecmbogota@users.noreply.github.com>
2024-03-08 08:35:16 -0500

    Update index.js

--- back/index.js
+++ back/index.js
@@ -19,7 +19,7 @@ const SECRET_KEY = process.env.SECRET_KEY || "TuCadenaSecreta";
 
 // Configuración de CORS
 const corsOptions = {
-  origin: ["http://caraballo.pro", "http://localhost:9000", "http://192.168.0.50:9000"],
+  origin: ["*"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
